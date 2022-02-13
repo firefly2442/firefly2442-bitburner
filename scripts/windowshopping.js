89e10063082dfde8da6336f19d92d3800e1b35ce
firefly2442 <firefly2442@gmail.com>
2022-02-13 10:03:48 -0700

    round display of costs up slightly

--- scripts/windowshopping.js
+++ scripts/windowshopping.js
@@ -3,7 +3,7 @@ export async function main(ns) {
 	let mymoney = ns.getServerMoneyAvailable("home")
 	let largestpurchase = 0
 	for (let i = 1; i <= 20; i++) {
-		ns.tprint(Math.pow(2, i) + " GB RAM -- $" + ns.getPurchasedServerCost(Math.pow(2, i)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+		ns.tprint(Math.pow(2, i) + " GB RAM -- $" + Math.ceil(ns.getPurchasedServerCost(Math.pow(2, i))).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
 		if (Number(mymoney) > Number(ns.getPurchasedServerCost(Math.pow(2, i)))) {
 			largestpurchase = Math.pow(2, i)
 		}
