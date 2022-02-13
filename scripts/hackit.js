29601675195e782e844a865f5fb9ce85e2e23d4e
firefly2442 <firefly2442@gmail.com>
2022-02-13 09:41:54 -0700

    only spin up one instance of hack/grow/weaken

--- scripts/hackit.js
+++ scripts/hackit.js
@@ -10,23 +10,33 @@ export async function main(ns) {
 	}
 
 	while(true) {
-		let freeramgb = ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname())
-		if (ns.getHostname() == "home") {freeramgb = freeramgb * 0.95} //reserve a little bit for other stuff
-		let availablegrowweakenthreads = Math.floor(freeramgb / ns.getScriptRam("/scripts/lib/grow.js"))
-		let availablehackthreads = Math.floor(freeramgb / ns.getScriptRam("/scripts/lib/hack.js"))
+		const ps = ns.ps(ns.getHostname())
+		let running = false
+		for (let script of ps) {
+			if (script.filename == "/scripts/lib/hack.js" || script.filename == "/scripts/lib/weaken.js" || script.filename == "/scripts/lib/grow.js") {
+				running = true
+			}
+		}
 
-		if (availablegrowweakenthreads == 0) {availablegrowweakenthreads++}
-		if (availablehackthreads == 0) {availablehackthreads++}
+		if (!running) {
+			let freeramgb = ns.getServerMaxRam(ns.getHostname()) - ns.getServerUsedRam(ns.getHostname())
+			if (ns.getHostname() == "home") {freeramgb = freeramgb * 0.95} //reserve a little bit for other stuff
+			let availablegrowweakenthreads = Math.floor(freeramgb / ns.getScriptRam("/scripts/lib/grow.js"))
+			let availablehackthreads = Math.floor(freeramgb / ns.getScriptRam("/scripts/lib/hack.js"))
 
-		if (ns.getServerSecurityLevel(data['target']) >= ns.getServerMinSecurityLevel(data['target']) * 0.99 &&
-			ns.getServerMoneyAvailable(data['target']) >= ns.getServerMaxMoney(data['target']) * 0.99) {
-			await ns.exec("/scripts/lib/hack.js", ns.getHostname(), availablehackthreads, "--target", data['target'])
-		} else if (ns.getServerSecurityLevel(data['target']) > ns.getServerMinSecurityLevel(data['target']) * 2) {
-			// TODO: could add more logic to tweak the number of threads to save RAM
-			await ns.exec("/scripts/lib/weaken.js", ns.getHostname(), availablegrowweakenthreads, "--target", data['target'])
-		} else {
-			// TODO: could add more logic to tweak the number of threads to save RAM
-			await ns.exec("/scripts/lib/grow.js", ns.getHostname(), availablegrowweakenthreads, "--target", data['target'])
+			if (availablegrowweakenthreads == 0) {availablegrowweakenthreads++}
+			if (availablehackthreads == 0) {availablehackthreads++}
+
+			if (ns.getServerSecurityLevel(data['target']) >= ns.getServerMinSecurityLevel(data['target']) * 0.99 &&
+				ns.getServerMoneyAvailable(data['target']) >= ns.getServerMaxMoney(data['target']) * 0.99) {
+				await ns.exec("/scripts/lib/hack.js", ns.getHostname(), availablehackthreads, "--target", data['target'])
+			} else if (ns.getServerSecurityLevel(data['target']) > ns.getServerMinSecurityLevel(data['target']) * 2) {
+				// TODO: could add more logic to tweak the number of threads to save RAM
+				await ns.exec("/scripts/lib/weaken.js", ns.getHostname(), availablegrowweakenthreads, "--target", data['target'])
+			} else {
+				// TODO: could add more logic to tweak the number of threads to save RAM
+				await ns.exec("/scripts/lib/grow.js", ns.getHostname(), availablegrowweakenthreads, "--target", data['target'])
+			}
 		}
 
 		await ns.sleep(10);
