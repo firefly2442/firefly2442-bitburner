939b7df01d5f454d1090cbd73cc6d97440a9b50c
firefly2442 <firefly2442@gmail.com>
2023-07-30 07:57:39 -0500

    add flag for upgrades

--- scripts/growhacknet.js
+++ scripts/growhacknet.js
@@ -1,16 +1,24 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	// $ run growhacknet.js --upgrade [true]
+	const data = ns.flags([
+		['upgrade', 'true']
+  	])
 
 	while(true){
 		for (var n = 0; n < ns.hacknet.numNodes(); n++) {
-			ns.hacknet.upgradeRam(n, 1)
-			ns.hacknet.upgradeLevel(n, 1)
-			ns.hacknet.upgradeCore(n, 1)
-			ns.hacknet.upgradeCache(n, 1)
+			if (data['upgrade'] == "true") {
+				ns.hacknet.upgradeRam(n, 1)
+				ns.hacknet.upgradeLevel(n, 1)
+				ns.hacknet.upgradeCore(n, 1)
+				ns.hacknet.upgradeCache(n, 1)
+			}
 		}
-		await ns.hacknet.purchaseNode()
-		await ns.hacknet.spendHashes("Sell for Money")
+		if (data['upgrade'] == "true") {
+			await ns.hacknet.purchaseNode()
+		}
+		while (ns.hacknet.spendHashes("Sell for Money")) {}
 
-		await ns.sleep(1000);
+		await ns.sleep(500);
 	}
 }
