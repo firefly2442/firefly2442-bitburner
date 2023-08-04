5d4e767f2e55e5e3daabbe20af5fe4aad595ff14
firefly2442 <firefly2442@gmail.com>
2023-08-03 20:40:57 -0500

    updates to corporate scripts for latest compatibility

--- scripts/corporate/runcorporation.js
+++ scripts/corporate/runcorporation.js
@@ -2,18 +2,22 @@
 export async function main(ns) {
     let cities = ["Aevum", "Sector-12", "Chongqing", "New Tokyo", "Ishima", "Volhaven"]
 
+    // create an agriculture division called "Agriculture"
+    // purchase the Warehouse API
+    // purchase the smart supply ability and configure it at the division level
+
     while (true) {
-        let corp = ns.corporation.getCorporation()    
+        let corp = ns.corporation.getCorporation()
         
         let upgradewarehouses = false
         for (let div of corp.divisions) {
             for (let city of cities) {
-                if (ns.corporation.hasWarehouse(div.name, city)) {
-                    let w = ns.corporation.getWarehouse(div.name, city)
+                if (ns.corporation.hasWarehouse(div, city)) {
+                    let w = ns.corporation.getWarehouse(div, city)
                     if (w.sizeUsed > w.size*.9) {
                         // purchase a warehouse upgrade if we can
-                        if (corp.funds > ns.corporation.getUpgradeWarehouseCost(div.name, city)) {
-                            ns.corporation.upgradeWarehouse(div.name, city)
+                        if (corp.funds > ns.corporation.getUpgradeWarehouseCost(div, city)) {
+                            ns.corporation.upgradeWarehouse(div, city)
                         } else {
                             upgradewarehouses = true
                         }
@@ -22,7 +26,8 @@ export async function main(ns) {
             }
         }
 
-        for (let div of ['Agriculture', 'Healthcare']) {
+        //for (let div of ['Agriculture', 'Healthcare']) {
+        for (let div of corp.divisions) {
             if (!upgradewarehouses) {
                 //ns.toast("Purchasing real-estate for " + div, "success", 1500)
                 for (let city of cities) {
@@ -30,7 +35,7 @@ export async function main(ns) {
                         let w = ns.corporation.getWarehouse(div, city)
                         if (w.sizeUsed < w.size*.9) {
                             let amnt = ns.corporation.getMaterial(div, city, "Real Estate")
-                            ns.corporation.buyMaterial(div, city, "Real Estate", Math.floor(amnt.qty*.001))
+                            ns.corporation.buyMaterial(div, city, "Real Estate", Math.floor(amnt.stored*.001))
                         }
                     }
                 }
